ba9f59ded04ce4890713094682af89164f6acbb3
vmigdal <volodymyr.migdal@gmail.com>
2019-01-31 18:46:04 +0200

    add js,bson tests

--- proto/dwtools/abase/l8.test/Gdf.test.s
+++ proto/dwtools/abase/l8.test/Gdf.test.s
@@ -82,9 +82,9 @@ function json( test )
 {
   var self = this;
 
-  /* */
+  /* json.fine */
 
-  test.case = 'select';
+  test.case = 'select json.fine';
 
   var serialize = _.Gdf.Select({ in : 'structure', out : 'string', ext : 'json', default : 1 });
   test.identical( serialize.length, 1 );
@@ -145,7 +145,49 @@ function json( test )
   // }
   // test.close( 'complicated' );
 
+
+  /* json.min */
+
+  test.case = 'select json.min';
+
+  var serialize = _.Gdf.Select({ in : 'structure', out : 'string', ext : 'json.min', default : 1 });
+  test.identical( serialize.length, 1 );
+  serialize = serialize[ 0 ];
+  test.identical( serialize.shortName, 'json.min' );
+
   /* */
+
+  test.open( 'simple' );
+  for( let s in SamplesSimple )
+  {
+    test.case = s;
+    let src = SamplesSimple[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'simple' );
+
+  /* */
+
+  test.open( 'primitive' );
+  for( let s in SamplesPrimitive )
+  {
+    test.case = s;
+    let src = SamplesPrimitive[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'primitive' );
 
 }
 
@@ -222,6 +264,189 @@ function cson( test )
 
 }
 
+//
+
+function js( test )
+{
+  var self = this;
+
+  /* */
+
+  test.case = 'select';
+
+  var serialize = _.Gdf.Select({ in : 'structure', out : 'string', ext : 'js' });
+  test.identical( serialize.length, 1 );
+  serialize = serialize[ 0 ];
+
+  var deserialize = _.Gdf.Select({ in : 'string', out : 'structure', ext : 'js' });
+  test.identical( deserialize.length, 1 );
+  deserialize = deserialize[ 0 ];
+
+  /* */
+
+  test.open( 'simple' );
+  for( let s in SamplesSimple )
+  {
+    test.case = s;
+    let src = SamplesSimple[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'simple' );
+
+  /* */
+
+  test.open( 'primitive' );
+  for( let s in SamplesPrimitive )
+  {
+    test.case = s;
+    let src = SamplesPrimitive[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'primitive' );
+
+  /* */
+
+  test.open( 'complicated' );
+  for( let s in SamplesComplicated )
+  {
+    test.case = s;
+    let src = SamplesComplicated[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'complicated' );
+
+  /* */
+
+}
+
+//
+
+function bson( test )
+{
+  var self = this;
+
+  /* */
+
+  test.case = 'select';
+
+  var serialize = _.Gdf.Select({ in : 'structure', out : 'buffer.node', ext : 'bson' });
+  test.identical( serialize.length, 1 );
+  serialize = serialize[ 0 ];
+
+  var deserialize = _.Gdf.Select({ in : 'buffer.node', out : 'structure', ext : 'bson' });
+  test.identical( deserialize.length, 1 );
+  deserialize = deserialize[ 0 ];
+
+  /* */
+
+  test.open( 'simple' );
+  for( let s in SamplesSimple )
+  {
+    test.case = s;
+    let src = SamplesSimple[ s ];
+
+    if( !_.mapIs( src ) )
+    src = { [ s ] : src };
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'buffer.node' );
+    test.is( _.bufferNodeIs( serialized.data ) );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'simple' );
+
+  test.case = 'all simple together';
+  var serialized = serialize.encode({ data : SamplesSimple });
+  test.identical( serialized.format, 'buffer.node' );
+  test.is( _.bufferNodeIs( serialized.data ) );
+
+  var deserialized = deserialize.encode({ data : serialized.data });
+  test.identical( deserialized.data, SamplesSimple );
+  test.identical( deserialized.format, 'structure' );
+
+  /* */
+
+  test.open( 'primitive' );
+  for( let s in SamplesPrimitive )
+  {
+    test.case = s;
+    let src = SamplesPrimitive[ s ];
+
+    if( !_.mapIs( src ) )
+    src = { [ s ] : src };
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'buffer.node' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'primitive' );
+
+  test.case = 'all primitive together';
+  var serialized = serialize.encode({ data : SamplesPrimitive });
+  test.identical( serialized.format, 'buffer.node' );
+  test.is( _.bufferNodeIs( serialized.data ) );
+
+  var deserialized = deserialize.encode({ data : serialized.data });
+  test.identical( deserialized.data, SamplesPrimitive );
+  test.identical( deserialized.format, 'structure' );
+
+  /* */
+
+  test.open( 'complicated' );
+  for( let s in SamplesComplicated )
+  {
+    test.case = s;
+    let src = SamplesComplicated[ s ];
+
+    if( !_.mapIs( src ) )
+    src = { [ s ] : src };
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'buffer.node' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'complicated' );
+
+  test.case = 'all complicated together';
+  var serialized = serialize.encode({ data : SamplesComplicated });
+  test.identical( serialized.format, 'buffer.node' );
+  test.is( _.bufferNodeIs( serialized.data ) );
+
+  var deserialized = deserialize.encode({ data : serialized.data });
+  test.identical( deserialized.data, SamplesComplicated );
+  test.identical( deserialized.format, 'structure' );
+
+  /* */
+
+}
+
 // --
 // declare
 // --
@@ -237,6 +462,8 @@ var Self =
     trivial,
     json,
     cson,
+    js,
+    bson
   },
 
 };
