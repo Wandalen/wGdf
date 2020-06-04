1cc75cae4552e91c0a5a1ec7b6a7d011f94352f5
ArtemBohush <art.bohush@gmail.com>
2020-06-04 19:14:24 +0300

    before pull

--- proto/dwtools/amid/l1/gdf/Json.s
+++ proto/dwtools/amid/l1/gdf/Json.s
@@ -30,107 +30,7 @@
   let _ = _global_.wTools;
   let Self = _global_.wTools;
   
-  // --
-  // json
-  // --
-  
-  let jsonSupported =
-  {
-    primitive : 1,
-    regexp : 0,
-    buffer : 0,
-    structure : 2
-  }
-  
-  let readJson =
-  {
-  
-    ext : [ 'json' ],
-    in : [ 'string' ],
-    out : [ 'structure' ],
-  
-    supporting : jsonSupported,
-  
-    onEncode : function( op )
-    {
-  
-  /*
-  
-  qqq : could throw different errors
-  cover them all please
-  
-  SyntaxError: Unexpected end of JSON input
-      at JSON.parse (<anonymous>:null:null)
-      at wGenericDataFormatConverter.onEncode (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\GdfFormats.s:59:26)
-      at wGenericDataFormatConverter.encode_body (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\Converter.s:238:13)
-      at Proxy.encode_body (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\GdfCurrent.s:59:45)
-  
-  */
-      try
-      {
-        op.out.data = JSON.parse( op.in.data );
-      }
-      catch( err )
-      {
-        let src = op.in.data;
-        let position = /at position (\d+)/.exec( err.message );
-        if( position )
-        position = Number( position[ 1 ] );
-        let first = 0;
-        // if( !isNaN( position ) )
-        if( position >= 0 )
-        {
-          let nearest = _.strLinesNearest( src, position );
-          first = _.strLinesCount( src.substring( 0, nearest.spans[ 0 ] ) );
-          src = nearest.splits.join( '' );
-        }
-        let err2 = _.err( 'Error parsing JSON\n', err, '\n', _.strLinesNumber( src, first ) );
-        throw err2;
-      }
-  
-      // op.out.data = _.jsonParse( op.in.data );
-      op.out.format = 'structure';
-  
-    },
-  
-  }
-  
-  let writeJsonMin =
-  {
-    default : 1,
-    ext : [ 'json.min', 'json' ],
-    shortName : 'json.min',
-    in : [ 'structure' ],
-    out : [ 'string' ],
-  
-    supporting : jsonSupported,
-  
-    onEncode : function( op )
-    {
-      op.out.data = JSON.stringify( op.in.data );
-      op.out.format = 'string';
-    }
-  
-  }
-  
-  let writeJsonFine =
-  {
-  
-    shortName : 'json.fine',
-    ext : [ 'json.fine', 'json' ],
-    in : [ 'structure' ],
-    out : [ 'string' ],
-  
-    supporting : jsonSupported,
-  
-    onEncode : function( op )
-    {
-      op.out.data = _.cloneData({ src : op.in.data });
-      op.out.data = _.toJson( op.out.data, { cloning : 0 } );
-      op.out.format = 'string';
-    }
-  
-  }
+ // ----------------------------------------
   
   // --
   // js
@@ -1132,6 +1032,17 @@
   // --
   
   _.Gdf([ readJson, writeJsonMin, writeJsonFine ]);
+  _.Gdf([ readJsStructure, /*readJsNode, readJsSmart, */ writeJsStrcuture ]);
+  _.Gdf([ readYml, writeYml ]);
+  _.Gdf([ readCoffee, writeCoffee ]);
+  _.Gdf([ readBson, writeBson ]);
+  _.Gdf([ readCbor, writeCbor ]);
+  _.Gdf([ readMsgpackLite, writeMsgpackLite, readMsgpackWtp, writeMsgpackWtp ]);
+  
+  _.Gdf([ base64ToBuffer, base64FromBuffer ]);
+  _.Gdf([ base64ToBlob ]);
+  _.Gdf([ base64FromUtf8Slow, base64ToUtf8Slow, base64FromUtf8, base64ToUtf8 ]);
+  _.Gdf([ utf8FromBuffer, utf8ToBuffer ]);
   
   // --
   // export
@@ -1140,5 +1051,111 @@
   if( typeof module !== 'undefined' )
   module[ 'exports' ] = Self;
   
-  })();
+})();
   
+
+    // --
+  // json
+  // --
+  
+  let jsonSupported =
+  {
+    primitive : 1,
+    regexp : 0,
+    buffer : 0,
+    structure : 2
+  }
+  
+  let readJson =
+  {
+  
+    ext : [ 'json' ],
+    in : [ 'string' ],
+    out : [ 'structure' ],
+  
+    supporting : jsonSupported,
+  
+    onEncode : function( op )
+    {
+  
+  /*
+  
+  qqq : could throw different errors
+  cover them all please
+  
+  SyntaxError: Unexpected end of JSON input
+      at JSON.parse (<anonymous>:null:null)
+      at wGenericDataFormatConverter.onEncode (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\GdfFormats.s:59:26)
+      at wGenericDataFormatConverter.encode_body (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\Converter.s:238:13)
+      at Proxy.encode_body (C:\pro\web\Dave\git\trunk\builder\include\dwtools\abase\l8\GdfCurrent.s:59:45)
+  
+  */
+      try
+      {
+        op.out.data = JSON.parse( op.in.data );
+      }
+      catch( err )
+      {
+        let src = op.in.data;
+        let position = /at position (\d+)/.exec( err.message );
+        if( position )
+        position = Number( position[ 1 ] );
+        let first = 0;
+        // if( !isNaN( position ) )
+        if( position >= 0 )
+        {
+          let nearest = _.strLinesNearest( src, position );
+          first = _.strLinesCount( src.substring( 0, nearest.spans[ 0 ] ) );
+          src = nearest.splits.join( '' );
+        }
+        let err2 = _.err( 'Error parsing JSON\n', err, '\n', _.strLinesNumber( src, first ) );
+        throw err2;
+      }
+  
+      // op.out.data = _.jsonParse( op.in.data );
+      op.out.format = 'structure';
+  
+    },
+  
+  }
+  
+  let writeJsonMin =
+  {
+    default : 1,
+    ext : [ 'json.min', 'json' ],
+    shortName : 'json.min',
+    in : [ 'structure' ],
+    out : [ 'string' ],
+  
+    supporting : jsonSupported,
+  
+    onEncode : function( op )
+    {
+      op.out.data = JSON.stringify( op.in.data );
+      op.out.format = 'string';
+    }
+  
+  }
+  
+  let writeJsonFine =
+  {
+  
+    shortName : 'json.fine',
+    ext : [ 'json.fine', 'json' ],
+    in : [ 'structure' ],
+    out : [ 'string' ],
+  
+    supporting : jsonSupported,
+  
+    onEncode : function( op )
+    {
+      op.out.data = _.cloneData({ src : op.in.data });
+      op.out.data = _.toJson( op.out.data, { cloning : 0 } );
+      op.out.format = 'string';
+    }
+  
+  }
+
+  //
+  //
+  //
