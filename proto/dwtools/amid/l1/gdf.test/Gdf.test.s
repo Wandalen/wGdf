fa871919c78f4a5ce3a69fbf407ae37ab81e292c
ArtemBohush <art.bohush@gmail.com>
2020-06-08 17:24:43 +0300

    registerAndFinit and supportedTypes test separated

--- proto/dwtools/amid/l1/gdf.test/Gdf.test.s
+++ proto/dwtools/amid/l1/gdf.test/Gdf.test.s
@@ -19,6 +19,8 @@ if( typeof module !== 'undefined' )
   var jsonFine = require( './JsonFine.test.s' ).tests.jsonFine;
   var utf8 = require( './Utf8.test.s' ).tests.utf8;
   var base64 = require( './Base64.test.s' ).tests.base64;
+  var supportedTypes = require( './SupportedTypes.test.s' ).tests.supportedTypes;
+  var registerAndFinit = require( './RegisterAndFinit.test.s' ).tests.registerAndFinit;
 }
 
 var _global = _global_;
@@ -30,94 +32,6 @@ debugger;
 // --
 // data
 // --
-
-let SamplesPrimitive =
-{
-
-  null : null,
-  number : 13,
-  string : 'something',
-
-}
-
-let SamplesSimple =
-{
-
-  map : { a : '1', dir : { b : 2 }, c : [ 1,2,3 ] },
-  array : [ { a : '1', dir : { b : 2 }, c : [ 1,2,3 ] } ],
-
-}
-
-let SamplesComplicated =
-{
-
-  regexp : /.regexp/g,
-  infinity : -Infinity,
-  nan : NaN,
-  date : new Date(),
-
-}
-
-//
-
-let Converters =
-{
-  'bson' :
-  {
-    serialize : { in : 'structure', out : 'buffer.node', ext : 'bson' },
-    deserialize : { in : 'buffer.node', out : 'structure', ext : 'bson' }
-  },
-
-  'json.fine' :
-  {
-    serialize : { in : 'structure', out : 'string', ext : 'json.fine' },
-    deserialize : { in : 'string', out : 'structure', ext : 'json', default : 1 }
-  },
-
-  'json' :
-  {
-    serialize : { in : 'structure', out : 'string', ext : 'json', default : 1 },
-    deserialize : { in : 'string', out : 'structure', ext : 'json', default : 1 }
-  },
-
-  'cson' :
-  {
-    serialize : { in : 'structure', out : 'string', ext : 'cson' },
-    deserialize : { in : 'string', out : 'structure', ext : 'cson' }
-  },
-
-  'js' :
-  {
-    serialize : { in : 'structure', out : 'string', ext : 'js' },
-    deserialize : { in : 'string', out : 'structure', ext : 'js' }
-  },
-
-  'cbor' :
-  {
-    serialize : { in : 'structure', out : 'buffer.node', ext : 'cbor' },
-    deserialize : { in : 'buffer.node', out : 'structure', ext : 'cbor' }
-  },
-
-  'yml' :
-  {
-    serialize : { in : 'structure', out : 'string', ext : 'yml' },
-    deserialize : { in : 'string', out : 'structure', ext : 'yml' }
-  },
-
-  // 'msgpack.lite' :
-  // {
-  //   serialize : { in : 'structure', out : 'buffer.node', ext : 'msgpack.lite' },
-  //   deserialize : { in : 'buffer.node', out : 'structure', ext : 'msgpack.lite' }
-  // },
-
-  // 'msgpack.wtp' :
-  // {
-  //   serialize : { in : 'structure', out : 'buffer.node', ext : 'msgpack.wtp' },
-  //   deserialize : { in : 'buffer.node', out : 'structure', ext : 'msgpack.wtp' }
-  // }
-}
-
-//
 
 /*
 qqq :
@@ -616,110 +530,6 @@ function select( test )
   test.shouldThrowErrorSync( () => _.Gdf.Select() );
   test.shouldThrowErrorSync( () => _.Gdf.Select({ ext : [ 'json.fine', 'json' ] }) );
 
-}
-
-//
-
-function registerAndFinit( test )
-{
-  let self = this;
-
-  var converter =
-  {
-    ext : [ 'ext' ],
-    in : [ 'string' ],
-    out : [ 'number' ],
-
-    onEncode : function( op )
-    {
-      _.assert( _.strIs( op.in.data ) );
-      op.out.data = Number.parseFloat( op.in.data );
-      op.out.format = 'number';
-    }
-  }
-
-  converter = _.Gdf( converter );
-
-  test.is( _.longHas( _.Gdf.Elements, converter ) );
-  test.is( _.longHas( _.Gdf.InMap[ 'string' ], converter ) );
-  test.is( _.longHas( _.Gdf.OutMap[ 'number' ], converter ) );
-  test.is( _.longHas( _.Gdf.ExtMap[ 'ext' ], converter ) );
-  test.is( _.longHas( _.Gdf.InOutMap[ 'string-number' ], converter ) );
-
-  converter.finit();
-
-  test.is( !_.longHas( _.Gdf.Elements, converter ) );
-  test.is( !_.longHas( _.Gdf.InMap[ 'string' ], converter ) );
-  test.is( !_.longHas( _.Gdf.OutMap[ 'number' ], converter ) );
-  test.is( !_.longHas( _.Gdf.ExtMap[ 'ext' ], converter ) );
-  test.is( !_.longHas( _.Gdf.InOutMap[ 'string-number' ], converter ) );
-}
-
-//
-
-function supportedTypes( test )
-{
-  var self = this;
-
-  let data = [];
-
-  for( let c in Converters )
-  {
-    let converter = Converters[ c ];
-
-    /* */
-
-    test.case = 'select';
-
-    var serialize = _.Gdf.Select( converter.serialize );
-    test.identical( serialize.length, 1 );
-    serialize = serialize[ 0 ];
-
-    var deserialize = _.Gdf.Select( converter.deserialize );
-    test.identical( deserialize.length, 1 );
-    deserialize = deserialize[ 0 ];
-
-    let options =
-    {
-      result :
-      {
-        primitive : 0,
-        regexp : 0,
-        buffer : 0,
-        structure : 0
-      },
-      serialize,
-      deserialize,
-      checks : {}
-    }
-
-    self.primitive1( test, options );
-    self.primitive2( test, options );
-    self.primitive3( test, options );
-    self.regExp1( test, options );
-    self.regExp2( test, options );
-    self.buffer1( test, options );
-    self.buffer2( test, options );
-    self.buffer3( test, options );
-    self.structure1( test, options );
-    self.structure2( test, options );
-    self.structure3( test, options );
-
-    test.contains( serialize.supporting, options.result );
-
-    let r = options.result;
-
-    data.push( [ serialize.ext, r.primitive, r.regexp, r.buffer, r.structure ] )
-  }
-
-  var o =
-  {
-    data,
-    head : [ 'Transformer','Primitive(0-3)','RegExp(0-2)','BufferNode(0-3)','Structure(0-3)' ],
-    colWidth : 20
-  }
-  var output = _.strTable( o );
-  console.log( output );
 }
 
 // --
---
+++ proto/dwtools/amid/l1/gdf.test/SupportedTypes.test.s
@@ -0,0 +1,166 @@
+( function _SupportedTypes_test_s_()
+{
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  var _ = require( '../../../../dwtools/Tools.s' );
+  require( '../gdf/Converter.s' );
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+_.assert( _testerGlobal_.wTools !== _global_.wTools );
+
+// --
+// test
+// --
+
+function supportedTypes( test )
+{
+  var self = this;
+
+  let Converters =
+  {
+    'bson' :
+    {
+      serialize : { in : 'structure', out : 'buffer.node', ext : 'bson' },
+      deserialize : { in : 'buffer.node', out : 'structure', ext : 'bson' }
+    },
+
+    'json.fine' :
+    {
+      serialize : { in : 'structure', out : 'string', ext : 'json.fine' },
+      deserialize : { in : 'string', out : 'structure', ext : 'json', default : 1 }
+    },
+
+    'json' :
+    {
+      serialize : { in : 'structure', out : 'string', ext : 'json', default : 1 },
+      deserialize : { in : 'string', out : 'structure', ext : 'json', default : 1 }
+    },
+
+    'cson' :
+    {
+      serialize : { in : 'structure', out : 'string', ext : 'cson' },
+      deserialize : { in : 'string', out : 'structure', ext : 'cson' }
+    },
+
+    'js' :
+    {
+      serialize : { in : 'structure', out : 'string', ext : 'js' },
+      deserialize : { in : 'string', out : 'structure', ext : 'js' }
+    },
+
+    'cbor' :
+    {
+      serialize : { in : 'structure', out : 'buffer.node', ext : 'cbor' },
+      deserialize : { in : 'buffer.node', out : 'structure', ext : 'cbor' }
+    },
+
+    'yml' :
+    {
+      serialize : { in : 'structure', out : 'string', ext : 'yml' },
+      deserialize : { in : 'string', out : 'structure', ext : 'yml' }
+    },
+
+    // 'msgpack.lite' :
+    // {
+    //   serialize : { in : 'structure', out : 'buffer.node', ext : 'msgpack.lite' },
+    //   deserialize : { in : 'buffer.node', out : 'structure', ext : 'msgpack.lite' }
+    // },
+
+    // 'msgpack.wtp' :
+    // {
+    //   serialize : { in : 'structure', out : 'buffer.node', ext : 'msgpack.wtp' },
+    //   deserialize : { in : 'buffer.node', out : 'structure', ext : 'msgpack.wtp' }
+    // }
+  }
+
+  let data = [];
+
+  for( let c in Converters )
+  {
+    let converter = Converters[ c ];
+
+    /* */
+
+    test.case = 'select';
+
+    var serialize = _.Gdf.Select( converter.serialize );
+    test.identical( serialize.length, 1 );
+    serialize = serialize[ 0 ];
+
+    var deserialize = _.Gdf.Select( converter.deserialize );
+    test.identical( deserialize.length, 1 );
+    deserialize = deserialize[ 0 ];
+
+    let options =
+    {
+      result :
+      {
+        primitive : 0,
+        regexp : 0,
+        buffer : 0,
+        structure : 0
+      },
+      serialize,
+      deserialize,
+      checks : {}
+    }
+
+    self.primitive1( test, options );
+    self.primitive2( test, options );
+    self.primitive3( test, options );
+    self.regExp1( test, options );
+    self.regExp2( test, options );
+    self.buffer1( test, options );
+    self.buffer2( test, options );
+    self.buffer3( test, options );
+    self.structure1( test, options );
+    self.structure2( test, options );
+    self.structure3( test, options );
+
+    test.contains( serialize.supporting, options.result );
+
+    let r = options.result;
+
+    data.push( [ serialize.ext, r.primitive, r.regexp, r.buffer, r.structure ] )
+  }
+
+  var o =
+  {
+    data,
+    head : [ 'Transformer','Primitive(0-3)','RegExp(0-2)','BufferNode(0-3)','Structure(0-3)' ],
+    colWidth : 20
+  }
+  var output = _.strTable( o );
+  console.log( output );
+}
+
+// --
+// declare
+// --
+
+var Self =
+{
+
+  name : 'Tools.supportedTypes.gdf',
+  silencing : 1,
+
+  context :
+  {
+  },
+
+  tests :
+  {
+    supportedTypes
+  },
+
+};
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = Self;
+
+} )();
