cfbe3cf43886864c675b91fde43005fc7d8bd76b
ArtemBohush <art.bohush@gmail.com>
2020-06-08 16:46:13 +0300

    Js and Cson test separated

---
+++ proto/dwtools/amid/l1/gdf.test/Cson.test.s
@@ -0,0 +1,136 @@
+( function _Cson_test_s_()
+{
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  var _ = require( '../../../../dwtools/Tools.s' );
+  require( '../gdf/Converter.s' );
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+_.assert( _testerGlobal_.wTools !== _global_.wTools );
+
+// --
+// test
+// --
+
+function cson( test )
+{
+  var self = this;
+
+  let SamplesPrimitive =
+  {
+    null : null,
+    number : 13,
+    string : 'something',
+  }
+
+  let SamplesSimple =
+  {
+    map : { a : '1', dir : { b : 2 }, c : [ 1, 2, 3 ] },
+    array : [ { a : '1', dir : { b : 2 }, c : [ 1, 2, 3 ] } ],
+  }
+
+  let SamplesComplicated =
+  {
+    regexp : /.regexp/g,
+    infinity : -Infinity,
+    nan : NaN,
+    date : new Date(),
+  }
+
+  /* */
+
+  test.case = 'select';
+
+  var serialize = _.Gdf.Select({ in : 'structure', out : 'string', ext : 'cson' });
+  test.identical( serialize.length, 1 );
+  serialize = serialize[ 0 ];
+
+  var deserialize = _.Gdf.Select({ in : 'string', out : 'structure', ext : 'cson' });
+  test.identical( deserialize.length, 1 );
+  deserialize = deserialize[ 0 ];
+
+  /* */
+
+  test.open( 'simple' );
+  for( let s in SamplesSimple )
+  {
+    test.case = s;
+    let src = SamplesSimple[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'simple' );
+
+  /* */
+
+  test.open( 'primitive' );
+  for( let s in SamplesPrimitive )
+  {
+    test.case = s;
+    let src = SamplesPrimitive[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'primitive' );
+
+  /* */
+
+  test.open( 'complicated' );
+  for( let s in SamplesComplicated )
+  {
+    test.case = s;
+    let src = SamplesComplicated[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'complicated' );
+
+  /* */
+
+}
+
+// --
+// declare
+// --
+
+var Self =
+{
+
+  name : 'Tools.cson.gdf',
+  silencing : 1,
+
+  context :
+  {
+  },
+
+  tests :
+  {
+    cson
+  },
+
+};
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = Self;
+
+} )();
---
+++ proto/dwtools/amid/l1/gdf.test/Js.test.s
@@ -0,0 +1,135 @@
+( function _Js_test_s_()
+{
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+  var _ = require( '../../../../dwtools/Tools.s' );
+  require( '../gdf/Converter.s' );
+}
+
+var _global = _global_;
+var _ = _global_.wTools;
+
+_.assert( _testerGlobal_.wTools !== _global_.wTools );
+
+// --
+// test
+// --
+
+function js( test )
+{
+  var self = this;
+
+  let SamplesPrimitive =
+  {
+    null : null,
+    number : 13,
+    string : 'something',
+  }
+
+  let SamplesSimple =
+  {
+    map : { a : '1', dir : { b : 2 }, c : [ 1, 2, 3 ] },
+    array : [ { a : '1', dir : { b : 2 }, c : [ 1, 2, 3 ] } ],
+  }
+
+  let SamplesComplicated =
+  {
+    regexp : /.regexp/g,
+    infinity : -Infinity,
+    nan : NaN,
+    date : new Date(),
+  }
+
+  /* */
+
+  test.case = 'select';
+
+  var serialize = _.Gdf.Select({ in : 'structure', out : 'string', ext : 'js' });
+  test.identical( serialize.length, 1 );
+  serialize = serialize[ 0 ];
+
+  var deserialize = _.Gdf.Select({ in : 'string', out : 'structure', ext : 'js' });
+  test.identical( deserialize.length, 1 );
+  deserialize = deserialize[ 0 ];
+
+  /* */
+
+  test.open( 'simple' );
+  for( let s in SamplesSimple )
+  {
+    test.case = s;
+    let src = SamplesSimple[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'simple' );
+
+  /* */
+
+  test.open( 'primitive' );
+  for( let s in SamplesPrimitive )
+  {
+    test.case = s;
+    let src = SamplesPrimitive[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'primitive' );
+
+  /* */
+
+  test.open( 'complicated' );
+  for( let s in SamplesComplicated )
+  {
+    test.case = s;
+    let src = SamplesComplicated[ s ];
+
+    var serialized = serialize.encode({ data : src });
+    test.identical( serialized.format, 'string' );
+
+    var deserialized = deserialize.encode({ data : serialized.data });
+    test.identical( deserialized.data, src );
+    test.identical( deserialized.format, 'structure' );
+  }
+  test.close( 'complicated' );
+
+  /* */
+
+}
+// --
+// declare
+// --
+
+var Self =
+{
+
+  name : 'Tools.js.gdf',
+  silencing : 1,
+
+  context :
+  {
+  },
+
+  tests :
+  {
+    js
+  },
+
+};
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = Self;
+
+} )();
